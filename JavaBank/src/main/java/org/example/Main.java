e705d8cdda190a98354d5c1a745b0fc2e37a1254
agco2002 <andrescapital.dev@gmail.com>
2025-06-23 22:24:28 -0500

    Tipos de datos primitivos

--- JavaBank/src/main/java/org/example/Main.java
+++ JavaBank/src/main/java/org/example/Main.java
@@ -3,7 +3,14 @@ package org.example;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hola mundo");
+
+        int numero_1 = 34;
+        int numero_2 = 40;
+        int resultado = numero_1 + numero_2;
+
+        System.out.print(resultado);
     }
+
 }
 
 
