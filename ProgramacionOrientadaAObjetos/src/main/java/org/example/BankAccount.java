cbc7f8a3ac0a87f70d217bf94c85a529f5a530b2
agco2002 <andrescapital.dev@gmail.com>
2025-07-27 22:19:33 -0500

    Bank Account V.2

--- ProgramacionOrientadaAObjetos/src/main/java/org/example/BankAccount.java
+++ ProgramacionOrientadaAObjetos/src/main/java/org/example/BankAccount.java
@@ -1,4 +1,5 @@
 package org.example;
+import java.util.Scanner;
 
 public class BankAccount {
    String AccountNumber;
@@ -9,7 +10,6 @@ public class BankAccount {
       this.AccountNumber = AccountNumber;
       this.holderName = holderName;
       this.balance = balance;
-   }
 
    //Deposit method
     public void deposit(double amount){
@@ -33,14 +33,50 @@ public class BankAccount {
       System.out.println("Current balance: $" + balance);
    }
 
+   //Display Menu
+   public void DisplayMenu(){
+       Scanner scanner = new Scanner(System.in);
+      int option;
+
+      do {
+         System.out.println("\n--- My Account Options ---");
+         System.out.println("1. Check balance");
+         System.out.println("2. Deposit money");
+         System.out.println("3. Withdraw money");
+         System.out.println("4. Exit");
+         System.out.print("Select an option: ");
+         option = scanner.nextInt();
+
+         switch (option) {
+            case 1:
+               ShowBalance();
+               break;
+            case 2:
+               System.out.print("Enter amount to deposit: ");
+               double depositAmount = scanner.nextDouble();
+               deposit(depositAmount);
+               break;
+            case 3:
+               System.out.print("Enter amount to withdraw: ");
+               double withdrawAmount = scanner.nextDouble();
+               withDraw(withdrawAmount);
+               break;
+            case 4:
+               System.out.println("Thank you. Exiting...");
+               break;
+            default:
+               System.out.println("Invalid option. Try again.");
+         }
+
+      } while(option != 4);
+
+      scanner.close();
+   }
 //Creating account object
 
    public static void main(String[] args){
       BankAccount MyAccount = new BankAccount("arl234588", "Andres Calderon", 1000000);
-      MyAccount.ShowBalance();
-      MyAccount.deposit(500000);
-      MyAccount.withDraw(250000);
-      MyAccount.ShowBalance();
+      MyAccount.DisplayMenu();
 
    }
 }
