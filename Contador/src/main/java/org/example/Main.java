4181d8c5ab179f0530f89d5927656664b2381aad
agco2002 <andrescapital.dev@gmail.com>
2025-10-14 21:53:43 -0500

    Contador

---
+++ Contador/src/main/java/org/example/Main.java
@@ -0,0 +1,15 @@
+package org.example;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        int numero = 5;
+
+        for (int i = 0; i <= 10; i++){
+            int operacion = numero * i;
+            System.out.println(numero + "x" + i + "=" + operacion);
+
+        }
+    }
+}
