15081be6f7c98a4f761f3a9e91b1cf82b907c76b
agco2002 <andrescapital.dev@gmail.com>
2025-10-14 21:12:32 -0500

    Comparacion

---
+++ Comparacion/src/main/java/org/example/Main.java
@@ -0,0 +1,17 @@
+package org.example;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+
+        int a = 13;
+        int b = 12;
+
+        if (a > b){
+            System.out.println(a + " es mayor que " + b);
+        }else {
+            System.out.println(b + " es mayor que " + a);
+        }
+    }
+}
