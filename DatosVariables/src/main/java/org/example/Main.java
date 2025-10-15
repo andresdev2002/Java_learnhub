f98272380e34b042431051719797be031dcc5790
agco2002 <andrescapital.dev@gmail.com>
2025-10-14 20:55:02 -0500

    DatosVariables

---
+++ DatosVariables/src/main/java/org/example/Main.java
@@ -0,0 +1,24 @@
+package org.example;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+
+        int edad = 22;
+        double valor = 15.000;
+        char inicial = 'A';
+        boolean activo = true;
+        String nombre = "Andres";
+
+        System.out.println("Hola mi nombre es " + nombre);
+        System.out.println("Tengo" + edad + " años");
+        System.out.println("La vocal inicial de mi nombre es " + inicial);
+        System.out.println("Esta información es:" + activo);
+        System.out.println("El costo de esta clase es de " + valor);
+
+
+
+
+    }
+}
