8c4d9b80b3d4d6ee4c46f9d53d11f02ed718ee7e
agco2002 <andrescapital.dev@gmail.com>
2025-10-15 21:03:40 -0500

    PersonClass

---
+++ Persona/src/main/java/org/example/Main.java
@@ -0,0 +1,14 @@
+package org.example;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args){
+        Persona persona1 = new Persona();
+        persona1.nombre = ("Andres");
+        persona1.edad = 22;
+        persona1.saludar();
+
+    }
+
+}
---
+++ Persona/src/main/java/org/example/Persona.java
@@ -0,0 +1,10 @@
+package org.example;
+
+public class Persona {
+    String nombre;
+    int edad;
+
+    void saludar(){
+        System.out.println("Hola mi nombre es " + nombre + " y tengo " + edad + "años" );
+    }
+}
